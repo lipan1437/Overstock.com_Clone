08ac383c431a5fe2eda67f61d0b27032d73fd314
lipan padhan <lipanpadhan1432@gmail.com>
2022-08-24 20:46:22 +0530

    changes

--- homepage/index.js
+++ homepage/index.js
@@ -29,6 +29,20 @@ let limitedTimeDeals = [
     text: "select lighting",
   },
 ];
+let append = (data) => {
+  let main = document.getElementById("main");
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = el.url;
+    let h3 = document.createElement("h3");
+    h3.innerText = el.text;
+    div.append(img, h3);
+    main.append(div);
+  });
+};
+append(limitedTimeDeals);
+
 let freshFinds = [
   {
     url: "https://ak1.ostkcdn.com/img/mxc/11192021-CatSilo-Furniture.png?imwidth=1920",
@@ -79,19 +93,6 @@ let freshFinds = [
     text: "Shop All Deals",
   },
 ];
-let append = (data) => {
-  let main = document.getElementById("main");
-  data.forEach((el) => {
-    let div = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = el.url;
-    let h3 = document.createElement("h3");
-    h3.innerText = el.text;
-    div.append(img, h3);
-    main.append(div);
-  });
-};
-append(limitedTimeDeals);
 let append2 = (data) => {
   let freshFind = document.getElementById("freshFind");
   data.forEach((el) => {
@@ -105,3 +106,44 @@ let append2 = (data) => {
   });
 };
 append2(freshFinds);
+ 
+let overStock = [
+        
+   {
+     url:"https://ak1.ostkcdn.com/img/mxc/C-Mod_HP_C_Mod_34762650.jpg?imwidth=1920",
+     discount:"Extra 20% off",
+     catagory:"Select Rugs by Artistic Weavers*"
+   },
+   {
+    url:"https://ak1.ostkcdn.com/img/mxc/HP_C_Mod_35950863.jpg?imwidth=1920",
+    discount:"Extra 20% off",
+    catagory:"Select Rugs by Balta*"
+  },
+  {
+    url:"https://ak1.ostkcdn.com/img/mxc/26986507_C_Mod.jpg?imwidth=1920",
+    discount:"Extra 10% off",
+    catagory:"Select Furniture by Ashley Furniture*"
+  },
+  {
+    url:"https://ak1.ostkcdn.com/img/mxc/C-Mod_HP_C_Mod_34762650.jpg?imwidth=1920",
+    discount:"Save On",
+    catagory:"Select Home Goods by Bus Furniture*"
+  },
+    
+]
+
+let append3 = (data) => {
+  let overStock = document.getElementById("overStock");
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = el.url;
+    let h4 = document.createElement('h4')
+    h4.innerText = el.discount
+    let p = document.createElement("p");
+    p.innerText = el.catagory;
+    div.append(img,h4, p);
+    overStock.append(div);
+  });
+};
+append3(overStock)

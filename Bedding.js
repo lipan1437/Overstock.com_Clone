e39612dd5d98148a963ee2ab2158725bd8eb2ffe
priyanshupawar <talk20priyanshu@gmail.com>
2022-08-26 01:39:32 +0530

    New file added

--- Bedding.js
+++ Bedding.js
@@ -1,126 +1,4 @@
-let kictchenArr = [
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_1_SmallAppliances.jpg",
-    title: "Small Appliances",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_2_Dinnerware.jpg",
-    title: "Dinnerware",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/03242022_SPRING2022_KITCHEN_PopCat_3.jpg",
-    title: "Cookware",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/10122021_KITCHEN_PopCat_3_Mixers.jpg",
-    title: " Mixers",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_4_CookingEssentials.jpg",
-    title: "Cooking Essentials",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_5_Serveware.jpg",
-    title: "Serveware",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_6_Bakeware.jpg",
-    title: "Bakeware",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-PopCat_7.jpg",
-    title: "Ranges & Ovens",
-  },
-
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_7_TableLinensandDecor.jpg",
-    title: "Table Linens & Decor",
-  },
-  {
-    Image:
-      "	https://ak1.ostkcdn.com/img/mxc/09062021_FALL2021_Kitchen_PopCat_7.jpg",
-    title: "Flatware",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-PopCat_8.jpg",
-    title: "Major Appliances",
-  },
-  {
-    Image:
-      "https://ak1.ostkcdn.com/img/mxc/12202021_WINTER2022_KITCHEN_PopCat_8_GlassesandBarware.jpg",
-    title: "Glasses & Barware",
-  },
-];
-let kictchenArr2 = [
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular7.jpg",
-    title: "Counter Accessories",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular8.jpg",
-    title: "Storage Canisters",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular9.jpg",
-    title: "Dish Racks",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular10.jpg",
-    title: "Trash Cans",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular11.jpg",
-    title: "Wine Racks",
-  },
-  {
-    image:
-      "	https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular12.jpg",
-    title: "Pot Racks",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular13.jpg",
-    title: "Islands",
-  },
-  {
-    image:
-      "	https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular14.jpg",
-    title: "Cabinets",
-  },
-  {
-    image:
-      "https://ak1.ostkcdn.com/img/mxc/03232020-gnp-kitchendining-175x175_otherPopular15.jpg",
-    title: "Butcher Blocks",
-  },
-  {
-    image: "",
-    title: "",
-  },
-  {
-    image: "",
-    title: "",
-  },
-  {
-    image: "",
-    title: "",
-  },
-];
-console.log(kictchenArr);
+// console.log(kictchenArr);
 let container = document.getElementById("container");
 let displayData = (kictchenArr) => {
   kictchenArr.forEach(({ Image, title }) => {
@@ -137,3 +15,56 @@ let displayData = (kictchenArr) => {
   });
 };
 // displayData(kictchenArr);
+let data = [
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/5aba8acb72afc85dbe29c44646d733ebfc1f957e/Maple-Quilt-Set.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/Maple-Quilt-Set/33912155/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=0",
+    selection2: "Maple Quilt Set",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/Maple-Quilt-Set/33912155/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=0",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/28117201/L33216692.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/Kasentex-Luxury-Reversible-Quilt-Set-with-Contemporary-Horizontal-Chevron-Design-Microfiber-Soft-Bedding/28117201/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=1",
+    selection2:
+      "Kasentex Reversible Quilt Set Chevron Design, Microfiber Soft Bedding with Shams",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/Kasentex-Luxury-Reversible-Quilt-Set-with-Contemporary-Horizontal-Chevron-Design-Microfiber-Soft-Bedding/28117201/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=1",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/980c7080b89f62fb4b4a21a6c1c5dbd1b568f221/Beach-Club-Reversible-Ultra-Soft-Microfiber-Quilt-Set.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/Beach-Club-Reversible-Ultra-Soft-Microfiber-Quilt-Set/31124959/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=2",
+    selection2: "Beach Club Coastal Reversible Ultra-Soft Microfiber Quilt Set",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/Beach-Club-Reversible-Ultra-Soft-Microfiber-Quilt-Set/31124959/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&osp=true&lyceumGuid=4b468885-f385-4fd6-879a-303e627eb1f7&searchidx=2",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/29b74603c736db047917c61add269c485e416506/Lugano-Honeycomb-Velvet-Oversized-Solid-Quilt-Set.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/Lugano-Honeycomb-Velvet-Oversized-Solid-Quilt-Set/31503528/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=3",
+    selection2: "Lugano Honeycomb Velvet Oversized Solid Quilt Set",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/Lugano-Honeycomb-Velvet-Oversized-Solid-Quilt-Set/31503528/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=3",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/8c033f3d3d4d203edb7b6ae4c4d17c8423ea6178/Serenta-9-pc.-Cal-King-Printed-Striped-Cotton-Blend-Bed-Coverlet-Set.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/Serenta-9-Piece-Printed-Striped-Cotton-Blend-Bedspread-Coverlet-Set/24251644/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=4",
+    selection2:
+      "Serenta 9-pc. Cal King Printed Striped Cotton Blend Bed Coverlet Set",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/Serenta-9-Piece-Printed-Striped-Cotton-Blend-Bedspread-Coverlet-Set/24251644/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=4",
+  },
+  {
+    image:
+      "https://ak1.ostkcdn.com/images/products/is/images/direct/431e28446ec716a2c5198cc39a610823a70fca8c/MarCielo-3-Piece-Printed-Quilt-Set-Lightweight-Bedspread-Set-By014.jpg?imwidth=480",
+    url: "https://www.overstock.com/Bedding-Bath/MarCielo-3-Piece-Printed-Quilt-Set-Lightweight-Bedspread-Set-By014/32906232/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=5",
+    selection2: "MarCielo 3-piece Printed Lightweight Bedspread Set",
+    selection2_url:
+      "https://www.overstock.com/Bedding-Bath/MarCielo-3-Piece-Printed-Quilt-Set-Lightweight-Bedspread-Set-By014/32906232/product.html?refccid=CXARMMVMFG32DB2JY6DO3AM7N4&searchidx=5",
+  },
+];
+console.log(data);

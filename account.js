481362c74704a71e1f2bb34998effce39adb7e9e
priyanshupawar <talk20priyanshu@gmail.com>
2022-08-27 00:43:08 +0530

    HTML and CSS

--- account.js
+++ account.js
@@ -1,14 +1,14 @@
 let userdata = JSON.parse(localStorage.getItem("signupData")) || [];
-  function Setdata() {
-    // event.preventDefault();
-    console.log("working");
-    let email = document.getElementById("Uemail").value;
-    let password = document.getElementById("Upassword").value;
-    let check = document.getElementById("Uc_password").value;
-    if (password !== check) {
-      alert("Enter the correct password in Confirm password");
-    } else {
-      let obj = {};
-    }
+function Setdata() {
+  // event.preventDefault();
+  console.log("working");
+  let email = document.getElementById("Uemail").value;
+  let password = document.getElementById("Upassword").value;
+  let check = document.getElementById("Uc_password").value;
+  if (password !== check) {
+    alert("Enter the correct password in Confirm password");
+  } else {
+    let obj = {};
   }
-  Setdata();
+}
+Setdata();

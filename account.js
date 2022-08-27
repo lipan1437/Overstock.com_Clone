ec2b21700967b83a68e995aa85241cf5e0969369
priyanshupawar <talk20priyanshu@gmail.com>
2022-08-27 12:47:13 +0530

    changes

--- account.js
+++ account.js
@@ -1,14 +1,50 @@
-let userdata = JSON.parse(localStorage.getItem("signupData")) || [];
-function Setdata() {
-  // event.preventDefault();
-  console.log("working");
-  let email = document.getElementById("Uemail").value;
-  let password = document.getElementById("Upassword").value;
-  let check = document.getElementById("Uc_password").value;
-  if (password !== check) {
-    alert("Enter the correct password in Confirm password");
+let signUpLS = JSON.parse(localStorage.getItem("signupData")) || [];
+
+document.querySelector("#creat").addEventListener("click", signupFunc);
+function signupFunc() {
+  let signupObj = {
+    Email: document.getElementById("Uemail").value,
+    Password: document.getElementById("Upassword").value,
+    Check: document.getElementById("Uc_password").value,
+  };
+
+  if (signupObj.Email.length > 0 && signupObj.Password.length > 0) {
+    alert("Signup Sucessful");
+  } else if (signupObj.Password.length < 5) {
+    alert("Enter atlest 6 char strong password");
+  } else if (signupObj.Password != signupObj.Check) {
+    alert("Enter the samne password in both");
+  }
+  signUpLS.push(signupObj);
+  localStorage.setItem("signupData", JSON.stringify(signUpLS));
+}
+
+// Signinpart start here
+let users = JSON.parse(localStorage.getItem("signupData")) || [];
+document.querySelector("#signin-btn").addEventListener("click", mySignIn);
+function mySignIn(event) {
+  event.preventDefault();
+  let data = {
+    userEmail: document.querySelector("#Iemail").value,
+    userPassword: document.querySelector("#Ipassword").value,
+  };
+
+  if (checkSignin(data.userEmail, data.userPassword) == true) {
+    localStorage.setItem("signinData", JSON.stringify(data));
+    alert("sign in successful");
+    window.location.href = "#";
   } else {
-    let obj = {};
+    alert("worng email or password");
+  }
+
+  function checkSignin(email, password) {
+    let filter = users.filter(function (ele) {
+      return ele.Email == email && ele.Password == password;
+    });
+    if (filter.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
-Setdata();

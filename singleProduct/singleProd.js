6199e40a18aa77aebc5fd20d4112d15215439459
lipan padhan <lipanpadhan1432@gmail.com>
2022-08-27 15:51:40 +0530

    singleProd page done

--- singleProduct/singleProd.js
+++ singleProduct/singleProd.js
@@ -2,25 +2,36 @@
 let kitenData = [
 
       {
-        url:"https://ak1.ostkcdn.com/images/products/is/images/direct/573c841ae5737d28b49df8d82d5eedf9b39fdea1/222-Fifth-Adelaide-16-Piece-Porcelain-Dinnerware-Set%2C-Blue.jpg?imwidth=480&impolicy=medium",
+       Image:"https://ak1.ostkcdn.com/images/products/is/images/direct/573c841ae5737d28b49df8d82d5eedf9b39fdea1/222-Fifth-Adelaide-16-Piece-Porcelain-Dinnerware-Set%2C-Blue.jpg?imwidth=480&impolicy=medium",
         price:"9601",
         title:"222 Fifth Adelaide 16-Piece Porcelain Dinnerware Set, Blue"
       },
-]
+]    
+
+let lsData = JSON.parse(localStorage.getItem('furniture')) || []
 let main = document.getElementById('show')
 let price = document.getElementById('price')
 let show = (data) => {    
     data.forEach((el)=>{
         let div = document.createElement('div')
         let img  = document.createElement('img')
-        img.src = el.url ;
+        img.src = el.Image ;
         let h3 = document.createElement('h3')
         h3.innerText=el.title ;
         let p = document.createElement('p')
         p.innerText =`Starting at INR ${el.price}`;
+        p.style.color = "red"
         div.append(img)
         main.append(div) 
         price.append(h3,p)
     })
 }
 show(kitenData)
+
+  let arr=[]
+ let addtoCart = () => {
+   let purchase = document.getElementById("addtoCart")
+     arr.push(purchase)
+   localStorage.setItem('cartItems',JSON.stringify(arr))
+  window.location.href="./cart.html"
+ }
